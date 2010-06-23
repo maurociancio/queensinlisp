1b1298f27855b03d4ce1ab7605d21246f9c5f1e3
Mauro Ciancio <maurociancio@gmail.com>
2010-06-23 16:50:03 -0300

    range

--- queens.lisp
+++ queens.lisp
@@ -5,6 +5,14 @@
 	)
 )
 
+
+(defun range (start end)
+	(if (> start end)
+		nil
+		(cons start (range (+ start 1) end))
+	)
+)
+
 ;=============================
 ;testing function
 ;=============================
@@ -23,3 +31,6 @@
 (test 'genrows1 (gen_rows 1 2) '((1 1) (1 2)))
 (test 'genrows2 (gen_rows 1 3) '((1 1) (1 2) (1 3)))
 (test 'genrows3 (gen_rows 2 3) '((2 1) (2 2) (2 3)))
+
+(test 'range1 (range 2 3) '(2 3))
+(test 'range2 (range 0 9) '(0 1 2 3 4 5 6 7 8 9))
